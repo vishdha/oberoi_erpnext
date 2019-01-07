10ee8f4350073e119444fc94a031284f6a667210
vishal <vishaldhayagude09@gmail.com>
2019-01-07 16:13:41 +0530

    [wip]Proforma Invoice notification added in Sales Order

--- oberoi_erpnext/api.py
+++ oberoi_erpnext/api.py
@@ -5,6 +5,7 @@ import frappe.utils
 from frappe.utils import cstr, flt, getdate, comma_and, cint
 from frappe import _
 from frappe.model.utils import get_fetch_values
+from frappe.desk.notifications import get_filters_for
 from frappe.model.mapper import get_mapped_doc
 from erpnext.stock.stock_balance import update_bin_qty, get_reserved_qty
 from frappe.desk.notifications import clear_doctype_notifications
@@ -59,3 +60,68 @@ def _make_sales_order(source_name, target_doc=None, ignore_permissions=False):
 
 	return doclist
 
+@frappe.whitelist()
+def get_open_count(doctype, name, links):
+	'''Get open count for given transactions and filters
+
+	:param doctype: Reference DocType
+	:param name: Reference Name
+	:param transactions: List of transactions (json/dict)
+	:param filters: optional filters (json/list)'''
+
+	frappe.has_permission(doc=frappe.get_doc(doctype, name), throw=True)
+
+	meta = frappe.get_meta(doctype)
+	#links = meta.get_dashboard_data()
+
+	links = frappe._dict({
+		'fieldname': 'prevdoc_docname',
+		'transactions': [
+			{
+				'label': _('Proforma Invoice'),
+				'items': ['Proforma Invoice']
+			},
+		]
+	})
+    #frappe.msgprint(str(links))
+    #links = frappe._dict(links)
+    #return {'count':0}
+
+
+	# compile all items in a list
+	items = []
+	for group in links.transactions:
+		items.extend(group.get('items'))
+
+	out = []
+	for d in items:
+		if d in links.get('internal_links', {}):
+			# internal link
+			continue
+
+		filters = get_filters_for(d)
+		fieldname = links.get('non_standard_fieldnames', {}).get(d, links.fieldname)
+        #return fieldname
+		data = {'name': d}
+		if filters:
+			# get the fieldname for the current document
+			# we only need open documents related to the current document
+			filters[fieldname] = name
+			total = len(frappe.get_all(d, fields='name',
+				filters=filters, limit=100, distinct=True, ignore_ifnull=True))
+			data['open_count'] = total
+
+		total = len(frappe.get_all(d, fields='name',
+			filters={fieldname: name}, limit=100, distinct=True, ignore_ifnull=True))
+		data['count'] = total
+		out.append(data)
+
+	out = {
+		'count': out,
+	}
+
+	module = frappe.get_meta_module(doctype)
+	if hasattr(module, 'get_timeline_data'):
+		out['timeline_data'] = module.get_timeline_data(doctype, name)
+    
+	return out
